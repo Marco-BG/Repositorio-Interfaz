15eba1325ef6857f165c5072404b391e434eba8c
Marco Benegas <180102@iessierradeguara.com>
2021-10-27 13:52:36 +0200

    subida de practica de otros controles

---
+++ OtrosControles/OtrosControles/Form1.Designer.vb
@@ -0,0 +1,224 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.MaskedTextBox1 = New System.Windows.Forms.MaskedTextBox()
+        Me.MonthCalendar1 = New System.Windows.Forms.MonthCalendar()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.miPopUp = New System.Windows.Forms.ToolTip(Me.components)
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.fileAbrir = New System.Windows.Forms.OpenFileDialog()
+        Me.btn_abrirArchivo = New System.Windows.Forms.Button()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        Me.TabControl1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"uno", "dos", "tres", "cuatro"})
+        Me.ComboBox1.Location = New System.Drawing.Point(175, 61)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 23)
+        Me.ComboBox1.TabIndex = 0
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Location = New System.Drawing.Point(338, 61)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(192, 23)
+        Me.DateTimePicker1.TabIndex = 1
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(607, 61)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(63, 15)
+        Me.LinkLabel1.TabIndex = 2
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "LinkLabel1"
+        '
+        'MaskedTextBox1
+        '
+        Me.MaskedTextBox1.Location = New System.Drawing.Point(175, 112)
+        Me.MaskedTextBox1.Mask = "#,000.00"
+        Me.MaskedTextBox1.Name = "MaskedTextBox1"
+        Me.MaskedTextBox1.Size = New System.Drawing.Size(100, 23)
+        Me.MaskedTextBox1.TabIndex = 4
+        '
+        'MonthCalendar1
+        '
+        Me.MonthCalendar1.Location = New System.Drawing.Point(338, 96)
+        Me.MonthCalendar1.Name = "MonthCalendar1"
+        Me.MonthCalendar1.TabIndex = 5
+        Me.miPopUp.SetToolTip(Me.MonthCalendar1, "Es un calendario.")
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Increment = New Decimal(New Integer() {10, 0, 0, 0})
+        Me.NumericUpDown1.Location = New System.Drawing.Point(175, 170)
+        Me.NumericUpDown1.Minimum = New Decimal(New Integer() {100, 0, 0, -2147483648})
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(120, 23)
+        Me.NumericUpDown1.TabIndex = 6
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.OtrosControles.My.Resources.Resources.analisisCame
+        Me.PictureBox1.Location = New System.Drawing.Point(60, 3)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(126, 129)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 7
+        Me.PictureBox1.TabStop = False
+        '
+        'Panel1
+        '
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel1.Controls.Add(Me.PictureBox1)
+        Me.Panel1.Location = New System.Drawing.Point(95, 212)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(231, 134)
+        Me.Panel1.TabIndex = 9
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Location = New System.Drawing.Point(607, 112)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(200, 100)
+        Me.GroupBox1.TabIndex = 10
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GroupBox1"
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Location = New System.Drawing.Point(362, 304)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.ShowToolTips = True
+        Me.TabControl1.Size = New System.Drawing.Size(200, 100)
+        Me.TabControl1.TabIndex = 11
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Location = New System.Drawing.Point(4, 24)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(192, 72)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Pestaña1"
+        Me.TabPage1.ToolTipText = "1"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Location = New System.Drawing.Point(4, 24)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(192, 72)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Pestaña 2"
+        Me.TabPage2.ToolTipText = "2"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(156, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(41, 15)
+        Me.Label1.TabIndex = 12
+        Me.Label1.Text = "Label1"
+        '
+        'fileAbrir
+        '
+        Me.fileAbrir.FileName = "abrir"
+        '
+        'btn_abrirArchivo
+        '
+        Me.btn_abrirArchivo.Location = New System.Drawing.Point(251, 12)
+        Me.btn_abrirArchivo.Name = "btn_abrirArchivo"
+        Me.btn_abrirArchivo.Size = New System.Drawing.Size(91, 23)
+        Me.btn_abrirArchivo.TabIndex = 13
+        Me.btn_abrirArchivo.Text = "Abrir Archivo"
+        Me.btn_abrirArchivo.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(940, 416)
+        Me.Controls.Add(Me.btn_abrirArchivo)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.MonthCalendar1)
+        Me.Controls.Add(Me.MaskedTextBox1)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.TabControl1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents MaskedTextBox1 As MaskedTextBox
+    Friend WithEvents MonthCalendar1 As MonthCalendar
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents miPopUp As ToolTip
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents Label1 As Label
+    Friend WithEvents fileAbrir As OpenFileDialog
+    Friend WithEvents btn_abrirArchivo As Button
+End Class
---
+++ OtrosControles/OtrosControles/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Friend WithEvents miReloj As Timer
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        miReloj = New Timer
+        miReloj.Interval = 1000
+        miReloj.Enabled = True
+
+    End Sub
+
+    Private Sub miReloj_Tick(sender As Object, e As EventArgs) Handles miReloj.Tick
+        Label1.Text = Now().ToLongTimeString
+
+    End Sub
+
+    Private Sub btn_abrirArchivo_Click(sender As Object, e As EventArgs) Handles btn_abrirArchivo.Click
+        fileAbrir.ShowDialog()
+    End Sub
+End Class
