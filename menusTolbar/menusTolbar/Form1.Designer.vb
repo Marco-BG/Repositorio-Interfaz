9e15093a1209c7914bba292ac3cad9b81a77d318
Marco Benegas <180102@iessierradeguara.com>
2021-11-02 10:00:10 +0100

    subo el tema de los menus y tolbars de interfaces

---
+++ menusTolbar/menusTolbar/Form1.Designer.vb
@@ -0,0 +1,195 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ArchivoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AbrirToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TextoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ImagenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GuardarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GuardarComoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SalirToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EdicionToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.VentanaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CascadaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HorizontalToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.VerticalToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.contextoMenu = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.AbrirCajaTextoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CerrarVentanaActivaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.contextoMenu.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ArchivoToolStripMenuItem, Me.EdicionToolStripMenuItem, Me.ToolStripMenuItem1, Me.VentanaToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.MdiWindowListItem = Me.VentanaToolStripMenuItem
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ArchivoToolStripMenuItem
+        '
+        Me.ArchivoToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AbrirToolStripMenuItem, Me.GuardarToolStripMenuItem, Me.GuardarComoToolStripMenuItem, Me.SalirToolStripMenuItem, Me.CerrarVentanaActivaToolStripMenuItem})
+        Me.ArchivoToolStripMenuItem.Name = "ArchivoToolStripMenuItem"
+        Me.ArchivoToolStripMenuItem.Size = New System.Drawing.Size(60, 20)
+        Me.ArchivoToolStripMenuItem.Text = "&Archivo"
+        '
+        'AbrirToolStripMenuItem
+        '
+        Me.AbrirToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.TextoToolStripMenuItem, Me.ImagenToolStripMenuItem})
+        Me.AbrirToolStripMenuItem.Name = "AbrirToolStripMenuItem"
+        Me.AbrirToolStripMenuItem.Size = New System.Drawing.Size(187, 22)
+        Me.AbrirToolStripMenuItem.Text = "Abrir"
+        '
+        'TextoToolStripMenuItem
+        '
+        Me.TextoToolStripMenuItem.Name = "TextoToolStripMenuItem"
+        Me.TextoToolStripMenuItem.Size = New System.Drawing.Size(114, 22)
+        Me.TextoToolStripMenuItem.Text = "Texto"
+        '
+        'ImagenToolStripMenuItem
+        '
+        Me.ImagenToolStripMenuItem.Name = "ImagenToolStripMenuItem"
+        Me.ImagenToolStripMenuItem.Size = New System.Drawing.Size(114, 22)
+        Me.ImagenToolStripMenuItem.Text = "Imagen"
+        '
+        'GuardarToolStripMenuItem
+        '
+        Me.GuardarToolStripMenuItem.Name = "GuardarToolStripMenuItem"
+        Me.GuardarToolStripMenuItem.Size = New System.Drawing.Size(187, 22)
+        Me.GuardarToolStripMenuItem.Text = "Guardar"
+        '
+        'GuardarComoToolStripMenuItem
+        '
+        Me.GuardarComoToolStripMenuItem.Name = "GuardarComoToolStripMenuItem"
+        Me.GuardarComoToolStripMenuItem.Size = New System.Drawing.Size(187, 22)
+        Me.GuardarComoToolStripMenuItem.Text = "Guardar Como"
+        '
+        'SalirToolStripMenuItem
+        '
+        Me.SalirToolStripMenuItem.Image = CType(resources.GetObject("SalirToolStripMenuItem.Image"), System.Drawing.Image)
+        Me.SalirToolStripMenuItem.Name = "SalirToolStripMenuItem"
+        Me.SalirToolStripMenuItem.Size = New System.Drawing.Size(187, 22)
+        Me.SalirToolStripMenuItem.Text = "Salir"
+        '
+        'EdicionToolStripMenuItem
+        '
+        Me.EdicionToolStripMenuItem.Name = "EdicionToolStripMenuItem"
+        Me.EdicionToolStripMenuItem.Size = New System.Drawing.Size(58, 20)
+        Me.EdicionToolStripMenuItem.Text = "Edicion"
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(24, 20)
+        Me.ToolStripMenuItem1.Text = "?"
+        '
+        'VentanaToolStripMenuItem
+        '
+        Me.VentanaToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CascadaToolStripMenuItem, Me.HorizontalToolStripMenuItem, Me.VerticalToolStripMenuItem})
+        Me.VentanaToolStripMenuItem.Name = "VentanaToolStripMenuItem"
+        Me.VentanaToolStripMenuItem.Size = New System.Drawing.Size(61, 20)
+        Me.VentanaToolStripMenuItem.Text = "Ventana"
+        '
+        'CascadaToolStripMenuItem
+        '
+        Me.CascadaToolStripMenuItem.Name = "CascadaToolStripMenuItem"
+        Me.CascadaToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.CascadaToolStripMenuItem.Text = "Cascada"
+        '
+        'HorizontalToolStripMenuItem
+        '
+        Me.HorizontalToolStripMenuItem.Name = "HorizontalToolStripMenuItem"
+        Me.HorizontalToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.HorizontalToolStripMenuItem.Text = "Horizontal"
+        '
+        'VerticalToolStripMenuItem
+        '
+        Me.VerticalToolStripMenuItem.Name = "VerticalToolStripMenuItem"
+        Me.VerticalToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.VerticalToolStripMenuItem.Text = "Vertical"
+        '
+        'contextoMenu
+        '
+        Me.contextoMenu.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AbrirCajaTextoToolStripMenuItem})
+        Me.contextoMenu.Name = "contextoMenu"
+        Me.contextoMenu.Size = New System.Drawing.Size(158, 26)
+        '
+        'AbrirCajaTextoToolStripMenuItem
+        '
+        Me.AbrirCajaTextoToolStripMenuItem.Name = "AbrirCajaTextoToolStripMenuItem"
+        Me.AbrirCajaTextoToolStripMenuItem.Size = New System.Drawing.Size(157, 22)
+        Me.AbrirCajaTextoToolStripMenuItem.Text = "Abrir Caja Texto"
+        '
+        'CerrarVentanaActivaToolStripMenuItem
+        '
+        Me.CerrarVentanaActivaToolStripMenuItem.Name = "CerrarVentanaActivaToolStripMenuItem"
+        Me.CerrarVentanaActivaToolStripMenuItem.Size = New System.Drawing.Size(187, 22)
+        Me.CerrarVentanaActivaToolStripMenuItem.Text = "Cerrar Ventana Activa"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ContextMenuStrip = Me.contextoMenu
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.IsMdiContainer = True
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.contextoMenu.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents ArchivoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AbrirToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents GuardarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents GuardarComoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SalirToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EdicionToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents TextoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ImagenToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents contextoMenu As ContextMenuStrip
+    Friend WithEvents AbrirCajaTextoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents VentanaToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CascadaToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HorizontalToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents VerticalToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CerrarVentanaActivaToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ menusTolbar/menusTolbar/Form1.vb
@@ -0,0 +1,36 @@
+﻿Public Class Form1
+    Private Sub SalirToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SalirToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+    Private Sub AbrirCajaTextoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AbrirCajaTextoToolStripMenuItem.Click
+
+
+    End Sub
+
+    Private Sub TextoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles TextoToolStripMenuItem.Click
+        Dim miHijo As New Hijo
+        miHijo.MdiParent = Me
+        miHijo.Show()
+
+    End Sub
+
+    Private Sub CascadaToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CascadaToolStripMenuItem.Click
+        Me.LayoutMdi(MdiLayout.Cascade)
+    End Sub
+
+    Private Sub HorizontalToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles HorizontalToolStripMenuItem.Click
+        Me.LayoutMdi(MdiLayout.TileHorizontal)
+    End Sub
+
+    Private Sub VerticalToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles VerticalToolStripMenuItem.Click
+        Me.LayoutMdi(MdiLayout.TileVertical)
+    End Sub
+
+    Private Sub CerrarVentanaActivaToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CerrarVentanaActivaToolStripMenuItem.Click
+        If Me.MdiChildren.Length = 0 Then
+            Exit Sub
+        End If
+        ActiveMdiChild.Close()
+    End Sub
+End Class
