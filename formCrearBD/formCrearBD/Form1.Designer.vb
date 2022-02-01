fc98fb7c4a0adcf593237458585aaeebe9f1a2df
Marquet <marcobenegasdev@outlook.es>
2022-02-02 00:41:29 +0100

    Crear base de datos en .net

---
+++ formCrearBD/formCrearBD/Form1.Designer.vb
@@ -0,0 +1,192 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.SampleDatabaseDataSet1 = New formCrearBD.SampleDatabaseDataSet1()
+        Me.SampleDatabaseDataSet1BindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.CustomerBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.CustomerTableAdapter = New formCrearBD.SampleDatabaseDataSet1TableAdapters.CustomerTableAdapter()
+        Me.TableAdapterManager = New formCrearBD.SampleDatabaseDataSet1TableAdapters.TableAdapterManager()
+        Me.CustomerIDDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.CompanyNameDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.ContactNameDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.PhoneDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.OrdersBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.OrdersTableAdapter = New formCrearBD.SampleDatabaseDataSet1TableAdapters.OrdersTableAdapter()
+        Me.OrdersDataGridView = New System.Windows.Forms.DataGridView()
+        Me.DataGridViewTextBoxColumn1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.SampleDatabaseDataSet1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.SampleDatabaseDataSet1BindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.CustomerBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.OrdersBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.OrdersDataGridView, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AutoGenerateColumns = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.CustomerIDDataGridViewTextBoxColumn, Me.CompanyNameDataGridViewTextBoxColumn, Me.ContactNameDataGridViewTextBoxColumn, Me.PhoneDataGridViewTextBoxColumn})
+        Me.DataGridView1.DataSource = Me.CustomerBindingSource
+        Me.DataGridView1.Location = New System.Drawing.Point(102, 24)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(440, 267)
+        Me.DataGridView1.TabIndex = 2
+        '
+        'SampleDatabaseDataSet1
+        '
+        Me.SampleDatabaseDataSet1.DataSetName = "SampleDatabaseDataSet1"
+        Me.SampleDatabaseDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'SampleDatabaseDataSet1BindingSource
+        '
+        Me.SampleDatabaseDataSet1BindingSource.DataSource = Me.SampleDatabaseDataSet1
+        Me.SampleDatabaseDataSet1BindingSource.Position = 0
+        '
+        'CustomerBindingSource
+        '
+        Me.CustomerBindingSource.DataMember = "Customer"
+        Me.CustomerBindingSource.DataSource = Me.SampleDatabaseDataSet1
+        '
+        'CustomerTableAdapter
+        '
+        Me.CustomerTableAdapter.ClearBeforeFill = True
+        '
+        'TableAdapterManager
+        '
+        Me.TableAdapterManager.BackupDataSetBeforeUpdate = False
+        Me.TableAdapterManager.CustomerTableAdapter = Me.CustomerTableAdapter
+        Me.TableAdapterManager.OrdersTableAdapter = Me.OrdersTableAdapter
+        Me.TableAdapterManager.UpdateOrder = formCrearBD.SampleDatabaseDataSet1TableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete
+        '
+        'CustomerIDDataGridViewTextBoxColumn
+        '
+        Me.CustomerIDDataGridViewTextBoxColumn.DataPropertyName = "CustomerID"
+        Me.CustomerIDDataGridViewTextBoxColumn.HeaderText = "CustomerID"
+        Me.CustomerIDDataGridViewTextBoxColumn.Name = "CustomerIDDataGridViewTextBoxColumn"
+        '
+        'CompanyNameDataGridViewTextBoxColumn
+        '
+        Me.CompanyNameDataGridViewTextBoxColumn.DataPropertyName = "CompanyName"
+        Me.CompanyNameDataGridViewTextBoxColumn.HeaderText = "CompanyName"
+        Me.CompanyNameDataGridViewTextBoxColumn.Name = "CompanyNameDataGridViewTextBoxColumn"
+        '
+        'ContactNameDataGridViewTextBoxColumn
+        '
+        Me.ContactNameDataGridViewTextBoxColumn.DataPropertyName = "ContactName"
+        Me.ContactNameDataGridViewTextBoxColumn.HeaderText = "ContactName"
+        Me.ContactNameDataGridViewTextBoxColumn.Name = "ContactNameDataGridViewTextBoxColumn"
+        '
+        'PhoneDataGridViewTextBoxColumn
+        '
+        Me.PhoneDataGridViewTextBoxColumn.DataPropertyName = "Phone"
+        Me.PhoneDataGridViewTextBoxColumn.HeaderText = "Phone"
+        Me.PhoneDataGridViewTextBoxColumn.Name = "PhoneDataGridViewTextBoxColumn"
+        '
+        'OrdersBindingSource
+        '
+        Me.OrdersBindingSource.DataMember = "Orders"
+        Me.OrdersBindingSource.DataSource = Me.SampleDatabaseDataSet1
+        '
+        'OrdersTableAdapter
+        '
+        Me.OrdersTableAdapter.ClearBeforeFill = True
+        '
+        'OrdersDataGridView
+        '
+        Me.OrdersDataGridView.AutoGenerateColumns = False
+        Me.OrdersDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.OrdersDataGridView.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.DataGridViewTextBoxColumn1, Me.DataGridViewTextBoxColumn2, Me.DataGridViewTextBoxColumn3, Me.DataGridViewTextBoxColumn4})
+        Me.OrdersDataGridView.DataSource = Me.OrdersBindingSource
+        Me.OrdersDataGridView.Location = New System.Drawing.Point(580, 51)
+        Me.OrdersDataGridView.Name = "OrdersDataGridView"
+        Me.OrdersDataGridView.Size = New System.Drawing.Size(300, 220)
+        Me.OrdersDataGridView.TabIndex = 2
+        '
+        'DataGridViewTextBoxColumn1
+        '
+        Me.DataGridViewTextBoxColumn1.DataPropertyName = "OrderID"
+        Me.DataGridViewTextBoxColumn1.HeaderText = "OrderID"
+        Me.DataGridViewTextBoxColumn1.Name = "DataGridViewTextBoxColumn1"
+        '
+        'DataGridViewTextBoxColumn2
+        '
+        Me.DataGridViewTextBoxColumn2.DataPropertyName = "CustomerID"
+        Me.DataGridViewTextBoxColumn2.HeaderText = "CustomerID"
+        Me.DataGridViewTextBoxColumn2.Name = "DataGridViewTextBoxColumn2"
+        '
+        'DataGridViewTextBoxColumn3
+        '
+        Me.DataGridViewTextBoxColumn3.DataPropertyName = "OrderDate"
+        Me.DataGridViewTextBoxColumn3.HeaderText = "OrderDate"
+        Me.DataGridViewTextBoxColumn3.Name = "DataGridViewTextBoxColumn3"
+        '
+        'DataGridViewTextBoxColumn4
+        '
+        Me.DataGridViewTextBoxColumn4.DataPropertyName = "OrderQuantity"
+        Me.DataGridViewTextBoxColumn4.HeaderText = "OrderQuantity"
+        Me.DataGridViewTextBoxColumn4.Name = "DataGridViewTextBoxColumn4"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(935, 477)
+        Me.Controls.Add(Me.OrdersDataGridView)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.SampleDatabaseDataSet1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.SampleDatabaseDataSet1BindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.CustomerBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.OrdersBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.OrdersDataGridView, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents SampleDatabaseDataSet1BindingSource As BindingSource
+    Friend WithEvents SampleDatabaseDataSet1 As SampleDatabaseDataSet1
+    Friend WithEvents CustomerBindingSource As BindingSource
+    Friend WithEvents CustomerTableAdapter As SampleDatabaseDataSet1TableAdapters.CustomerTableAdapter
+    Friend WithEvents TableAdapterManager As SampleDatabaseDataSet1TableAdapters.TableAdapterManager
+    Friend WithEvents CustomerIDDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents CompanyNameDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents ContactNameDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents PhoneDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents OrdersTableAdapter As SampleDatabaseDataSet1TableAdapters.OrdersTableAdapter
+    Friend WithEvents OrdersBindingSource As BindingSource
+    Friend WithEvents OrdersDataGridView As DataGridView
+    Friend WithEvents DataGridViewTextBoxColumn1 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn2 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn3 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn4 As DataGridViewTextBoxColumn
+End Class
---
+++ formCrearBD/formCrearBD/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        'TODO: esta línea de código carga datos en la tabla 'SampleDatabaseDataSet1.Orders' Puede moverla o quitarla según sea necesario.
+        Me.OrdersTableAdapter.Fill(Me.SampleDatabaseDataSet1.Orders)
+        'TODO: esta línea de código carga datos en la tabla 'SampleDatabaseDataSet1.Customer' Puede moverla o quitarla según sea necesario.
+        Me.CustomerTableAdapter.Fill(Me.SampleDatabaseDataSet1.Customer)
+
+
+    End Sub
+End Class
